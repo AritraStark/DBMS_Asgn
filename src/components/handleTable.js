8ef1d1e0883f5cb035a40863a766a5383a10cd55
Aritra Das <aritradas0303@gmail.com>
2022-02-25 13:37:21 +0530

    assignment 1 updated

--- src/components/handleTable.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { StudentTable } from "./studentTable";
-
-export const HandleTable = ({ type, data, showData }) => {
-    if(type==='search') return <StudentTable rows={showData}/>
-    else if(type==='showAll') return <StudentTable rows={data}/>
-    else return <StudentTable rows={data}/>
-}
